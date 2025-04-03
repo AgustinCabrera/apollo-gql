3a36c2f2d295a71d13aec89517d2e729e3f574fc
agustincabrera <agustinhkramer@gmail.com>
2025-04-03 14:53:56 -0300

    update

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,14 @@
-//TODO
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const typeDefs = require("./schema");
+
+const gql = require("graphql-tag");
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs });
+  const { url } = await startStandaloneServer(server);
+  console.log(`
+    🚀  Server is running!
+    📭  Query at ${url}
+  `);
+}
+startApolloServer();
